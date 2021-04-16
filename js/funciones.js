caa7cdd08cd82373be518c04a8d930d069350c4b
deison <deisontuiran@gmail.com>
2021-04-16 16:13:11 -0500

    actualizado

--- js/funciones.js
+++ js/funciones.js
@@ -1,29 +1,45 @@
 const productos = [];
-
+const tabla_prod = document.querySelector("#tabla-prod")
 const botones = document.querySelectorAll(".caja > button");
-
-console.log(botones);
-
+let contador = 0;
 
 
 
 
-/* botones.forEach((boton) => {
+botones.forEach((boton) => {
+    boton.addEventListener("click", (eveto) => {
+        eveto.preventDefault();
+        const listado = boton.parentElement;
+        const producto = {
+            imagen: listado.querySelector("img").src,
+            nombre: listado.querySelector("h3").textContent,
+            precio: listado.querySelector(".caja-precio").textContent,
+            cantidad: 1
+        }
+        aggProducto(producto);
+    })
 
-    boton.addEventListener("click", (evento)=>{
-     evento.preventDefault();
+})
+const aggProducto = (producto) => {
 
-    const tarjeta = boton.parentElement;
-
-    const producto = {
-
-        imagen: tarjeta.querySelector("img").src,
-        nombre: tarjeta.querySelector("h3").textContent,
-        precio: tarjeta.querySelector("p").textContent,
+    const mostrar = {
+        imagen: producto.querySelector("img").src,
+        nombre: producto.querySelector("h3").textContent,
+        precio: producto.querySelector(".caja-precio").textContent,
+        id: producto.querySelector(".btn").getAttribute("data-id"),
         cantidad: 1
     }
 
-    })
-})
- */
-   
+    this.insertar(mostrar);
+}
+insertarCarrito(mostrar){
+    const tabla = document.createElement("tr");
+    tabla.innerHTML = tabla.innerHTML + `
+                                <td> <img src"${producto.imagen}"></td>
+                                <td>${producto.nombre}</td>
+                                <td>${producto.precio}</td>
+                                <td>${nota2.value}</td>
+                                `;
+    tabla_prod.appendChild(tabla);
+}
+
