8fb35b34e4960a3529cb71f241128c1a67beb7ae
Deison27tl <deisontuiran2000@gmail.com>
2021-04-16 12:58:26 -0500

    primero

---
+++ js/funciones.js
@@ -0,0 +1,29 @@
+const productos = [];
+
+const botones = document.querySelectorAll(".caja > button");
+
+console.log(botones);
+
+
+
+
+
+/* botones.forEach((boton) => {
+
+    boton.addEventListener("click", (evento)=>{
+     evento.preventDefault();
+
+    const tarjeta = boton.parentElement;
+
+    const producto = {
+
+        imagen: tarjeta.querySelector("img").src,
+        nombre: tarjeta.querySelector("h3").textContent,
+        precio: tarjeta.querySelector("p").textContent,
+        cantidad: 1
+    }
+
+    })
+})
+ */
+   
